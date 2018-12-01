7127ad1cb4d94e397e4e720abe58464921464b48
DESKTOP-NST23KS\JuanLuis López <a1134239@uabc.edu.mx>
2018-12-01 00:24:28 -0800

    Solución de un error en un link (dialogflow)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,6 +112,7 @@
 allow="microphone;"
 width="350"
 height="430"
+ src="https://console.dialogflow.com/api-client/demo/embedded/cbe5f58e-edc2-4d66-8b88-ff4b06b82dc2">
 </iframe>
 </div>
     </body>
